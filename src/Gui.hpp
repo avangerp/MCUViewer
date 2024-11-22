7b237daafd131c2a734be1f7353b0e24bce02626
avangerp <andrew.vangerpen@gmail.com>
2024-11-21 23:06:24 -0600

    add metal example

---
+++ src/Gui.hpp
@@ -0,0 +1,6 @@
+#ifndef GUI_H
+#define GUI_H
+
+int Gui(int argc, char** argv);
+
+#endif // GUI_H
---
+++ src/main.mm
@@ -0,0 +1,19 @@
+// Dear ImGui: standalone example application for GLFW + Metal, using programmable pipeline
+// (GLFW is a cross-platform general purpose library for handling windows, inputs, OpenGL/Vulkan/Metal graphics context creation, etc.)
+
+// Learn about Dear ImGui:
+// - FAQ                  https://dearimgui.com/faq
+// - Getting Started      https://dearimgui.com/getting-started
+// - Documentation        https://dearimgui.com/docs (same as your local docs/ folder).
+// - Introduction, links and more at the top of imgui.cpp
+
+#include "imgui.h"
+#include "imgui_impl_glfw.h"
+#include "Gui.hpp"
+
+int main(int argc, char** argv)
+{
+    // Call Gui
+    return Gui(argc, argv);
+    
+}
