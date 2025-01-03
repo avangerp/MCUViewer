1f04a3b07417817eeb439927d41f1509762ce8d1
avangerp <andrew.vangerpen@gmail.com>
2025-01-03 08:29:27 -0600

    Add MCUViewer_test2 to examples. Supports F401RE Nucleo board that I can test on

---
+++ example/MCUViewer_test2/Core/App/main.hpp
@@ -0,0 +1,24 @@
+/*
+ * main.hpp
+ *
+ *  Created on: Feb 4, 2023
+ *      Author: klonyyy
+ */
+
+#ifndef APP_MAIN_HPP_
+#define APP_MAIN_HPP_
+
+#ifdef __cplusplus
+ extern "C" {
+#endif
+
+#include "main.h"
+
+void maincpp(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+
+#endif /* APP_MAIN_HPP_ */
